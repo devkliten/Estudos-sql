536b7676470634b7455d8009aea731454fbeac35
devkliten <144634676+devkliten@users.noreply.github.com>
2026-05-26 18:41:18 -0300

    Update queries_praticas.sql

--- queries_praticas.sql
+++ queries_praticas.sql
@@ -45,11 +45,22 @@ select nome as produto_nao_vendidos from produto
 left join item_venda
 on produto.id_produto = item_venda.id_produto
 where item_venda.id_item is null;
+--------------------------------------------------------------------------------------------------------------
+-- quantidade pedidos agrupados por status 
 
+select status, count(status) as quantidade from venda
+group by status 
+order by count(status) desc;
+--------------------------------------------------------------------------------------------------------------
+-- maiores valores de compras agrupadas pelo nome do cliente, sendo pelo menos acima de 5000
 
-
-
-
+select cliente.nome, max(venda.valor_total) as maior_compra
+from cliente inner join venda
+on cliente.id_cliente = venda.id_cliente 
+group by cliente.nome 
+having max(venda.valor_total)>5000
+order by max(venda.valor_total) desc;
+--------------------------------------------------------------------------------------------------------------
 
 
 
